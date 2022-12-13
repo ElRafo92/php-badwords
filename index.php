3264b847d1a6eba22b17854c0f0c604c8a8e9756
ElRafo92 <rafael.martinez@outlook.it>
2022-12-13 19:25:05 +0100

    esercizio completato

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php 
+    $text = "Farfallina bella e bianca
+    vola vola mai si stanca
+    vola qua, vola là:
+    mai nessun la fermerà!
+    vola, vola ore ed ore
+    poi si posa su un bel fiore
+    vola qua, vola la:
+    mai nessun la fermerà!";
+
+    $word = $_GET["word"];
+    
+    $text2 = str_replace($word, "***", $text);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +22,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>ciaone</h1>
+
+    <h1><?php echo $text ?></h1>
+    <h2>lunghezza testo: <?php echo strlen($text)?></h2>
+    <h1><?php echo $text2 ?></h1>
+    <h2>lunghezza testo: <?php echo strlen($text2)?></h2>
+
 </body>
 </html>
